031fcf1c20bced54c31701535181812963dc9d2e
choihanil <ccaca@ykp.kr>
2026-02-09 18:09:55 +0900

    fix: update datetime usage to use KST timezone for consistency

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import warnings, time, random, json, argparse, subprocess, requests
 from anyio import Path
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
-from datetime import datetime, timezone
+from datetime import datetime, timedelta, timezone
 
 from crawler.base import BaseCrawler
 
@@ -44,6 +44,7 @@ from config.vm_google_sheet_setting import (
 )
 
 warnings.filterwarnings("ignore", message=".*pin_memory.*")
+KST = timezone(timedelta(hours=9))
 
 # es = Elasticsearch(ES_HOST)
 
@@ -137,7 +138,7 @@ def run_naver(driver, keyword: str, debug: bool = False):
 def main():
     args = parse_args()
 
-    index_name = f"{ES_INDEX_PREFIX}-{datetime.now():%Y-%m-%d}"
+    index_name = f"{ES_INDEX_PREFIX}-{datetime.now(KST):%Y-%m-%d}"
 
     # 드라이버 분리 (정답 구조)
     naver_crawler = BaseCrawler()  # apt chromium
@@ -206,7 +207,7 @@ def main():
                     # 실패 행 기록
                     rows = [
                         [
-                            datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                            datetime.now(KST).strftime("%Y-%m-%d %H:%M:%S"),
                             VM_NAME,
                             elapsed_sec,
                             keyword,
