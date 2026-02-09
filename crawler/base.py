ed738206f671bb3e1421078f04a7463ae644fa32
choihanil <ccaca@ykp.kr>
2026-02-09 17:34:07 +0900

    refactor: comment out unused ChromeDriverManager import and related code

--- crawler/base.py
+++ crawler/base.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+
+# from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
@@ -38,17 +39,17 @@ class BaseCrawler:
         self.driver.quit()
 
 
-def create_google_driver():
-    options = webdriver.ChromeOptions()
+# def create_google_driver():
+#     options = webdriver.ChromeOptions()
 
-    options.add_argument("--headless")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--window-size=1200,900")
-    options.add_argument("--disable-blink-features=AutomationControlled")
+#     options.add_argument("--headless")
+#     options.add_argument("--no-sandbox")
+#     options.add_argument("--disable-dev-shm-usage")
+#     options.add_argument("--disable-gpu")
+#     options.add_argument("--window-size=1200,900")
+#     options.add_argument("--disable-blink-features=AutomationControlled")
 
-    return webdriver.Chrome(
-        service=Service(ChromeDriverManager().install()),
-        options=options,
-    )
+#     return webdriver.Chrome(
+#         service=Service(ChromeDriverManager().install()),
+#         options=options,
+#     )
