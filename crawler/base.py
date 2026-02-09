590d3cf71a770bbf6bfe64639ad3297e94333cb1
choihanil <ccaca@ykp.kr>
2026-02-09 17:42:22 +0900

    feat: update BaseCrawler to use snap chromium and remove chromedriver path specification

--- crawler/base.py
+++ crawler/base.py
@@ -8,16 +8,16 @@ from selenium.webdriver.chrome.options import Options
 class BaseCrawler:
     """
     NAVER 전용 크롤러
-    (apt chromium + 서버 안정화 최종본)
+    (snap chromium + Selenium Manager 최종본)
     """
 
     def __init__(self):
         options = Options()
 
-        # apt chromium
-        options.binary_location = "/usr/bin/chromium-browser"
+        # [변경] apt chromium 경로 → snap chromium 경로
+        options.binary_location = "/snap/bin/chromium"
 
-        # 서버 필수 옵션 (최소)
+        # 서버 필수 옵션
         options.add_argument("--headless=new")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
@@ -25,7 +25,8 @@ class BaseCrawler:
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--window-size=1200,900")
 
-        service = Service(executable_path="/usr/bin/chromedriver")
+        # [변경] chromedriver 경로 지정 제거
+        service = Service()  # Selenium Manager 사용
 
         self.driver = webdriver.Chrome(
             service=service,
